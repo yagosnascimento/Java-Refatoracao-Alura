d40f4cc7c102fbfbbc68a468766339e956a2b33d
Yago Nascimento <yagosiqueira450@gmail.com>
2026-07-02 14:23:16 -0300

    Continuando curso.

---
+++ src/main/java/br/com/alura/domain/Abrigo.java
@@ -0,0 +1,14 @@
+package br.com.alura.domain;
+
+public class Abrigo {
+
+    private String nome;
+    private String telefone;
+    private String email;
+
+    public Abrigo(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+}
---
+++ src/main/java/br/com/alura/domain/Pet.java
@@ -0,0 +1,10 @@
+package br.com.alura.domain;
+
+public class Pet {
+    private String tipo;
+    private String nome;
+    private String raca;
+    private int idade;
+    private String cor;
+    private Float peso;
+}
